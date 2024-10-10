e9d9e1c737d07f6395d01d57bced535bc50d6262
Gokul Gc <147521846+Gokulgc37@users.noreply.github.com>
2024-10-10 16:07:08 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -29,7 +29,7 @@ async function makePrediction(event) {
     };
 
     // Make the POST request to FastAPI
-    const response = await fetch("http://3.101.45.77:8080/predict", { //if we run in on cloud use http://our-ec2-public-ip:8080/predict
+    const response = await fetch("http://54.241.255.215:8080/predict", { //if we run in on cloud use http://our-ec2-public-ip:8080/predict
         method: "POST",
         headers: {
             "Content-Type": "application/json"
